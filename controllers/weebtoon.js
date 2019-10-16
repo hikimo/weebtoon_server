a688e0cc167de0bf6b860f849b0444d40bfb3f71
Rangga Prastio <ranggaprastio123@gmail.com>
2019-10-16 09:14:47 +0700

    adding search

--- controllers/weebtoon.js
+++ controllers/weebtoon.js
@@ -1,9 +1,27 @@
 const models = require('../models')
 const Manga = models.manga 
+const sequelize = require('sequelize')
+const Op = sequelize.Op
 
 exports.index = (req, res) => {
-  Manga.findAll().then(mangas => {
-    res.send({mangas})
-  })
+  const name = req.query.name
+  if(name) {
+    Manga.findAll({
+      where: { 
+        name: { [Op.like] : `%${name}%` } 
+      }
+    }).then(mangas => {
+      if(mangas.length > 0)
+        res.send(mangas)
+      else
+        res.send({status: 'failed', message: 'There\'s no manga founded using that name'})
+    })
+  } else {
+    Manga.findAll().then(mangas => {
+      if(mangas.length > 0)
+        res.send(mangas)
+      else
+        res.send({status: 'failed', message: 'There\'s no manga found'})
+    })
+  }
 }
-
